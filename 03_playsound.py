4ba8c846bfea06c80103b5622473b0f98e43a744
Rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2020-11-02 18:18:01 +0600

    chapter 1

---
+++ 03_playsound.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('E:\\CODE\\python\\python2020\\song.mp3')
---
+++ 04_listdir.py
@@ -0,0 +1,7 @@
+# author : rokib
+# location : bd
+# date : 2020/11/2
+
+import os
+
+print(os.listdir())
